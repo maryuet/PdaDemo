0c3f9cdb2aceecb3e0990bf63b38c6503a682777
maryuet <782256088@qq.com>
2016-11-30 22:22:46 +0800

    complete product select and product tree

--- taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.taotao.common.pojo.EasyUIDataGridResult;
 import com.taotao.pojo.TbItem;
 import com.taotao.service.ItemService;
 
@@ -20,5 +21,12 @@ public class ItemController {
 	private TbItem getItemById(@PathVariable Long itemId) {
 		TbItem item = itemService.getItemById(itemId);
 		return item;
+	}
+	
+	@RequestMapping("/item/list")
+	@ResponseBody
+	public EasyUIDataGridResult getItemList(Integer page, Integer rows) {
+		EasyUIDataGridResult result = itemService.getItemList(page, rows);
+		return result;
 	}
 }
